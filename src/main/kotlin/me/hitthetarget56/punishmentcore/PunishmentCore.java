e4e062b297693e99a1d65e6365ffa117001a0300
Hitthetarget55 <46300928+Hitthetarget55@users.noreply.github.com>
2021-07-31 18:47:36 +0100

    initial

---
+++ src/main/kotlin/me/hitthetarget56/punishmentcore/PunishmentCore.java
@@ -0,0 +1,17 @@
+package me.hitthetarget56.punishmentcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PunishmentCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
